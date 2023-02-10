6a612086b1986e95b612158a1f0e8f3589370d3e
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-02-10 21:53:33 +0330

    set folders and add pictures, icons and colors

---
+++ lib/constants/my_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class SolidColor {
+  static const primaryColor = Color(0xffD17842);
+  static const secondaryColor = Color(0xffD17842);
+  static const primaryTextColor = Color(0xffffffff);
+  static const secondaryTextColor = Color(0xff444649);
+  static const backgroundColor = Color(0xff0C0F14);
+}
